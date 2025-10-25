42568dbad0045759b97d94dd3621f57940d6c7c3
jaki <jacopocarrani@gmail.com>
2025-10-25 02:01:34 +0100

    remove deprecated

--- .deprecated/core/scraping/__init__.py
+++
@@ -1,9 +0,0 @@
-from .google import GoogleHandler
-from .soundcloud import SoundCloudHandler
-from .tracklist import extract_tracklist
-
-__all__ = [
-    "GoogleHandler",
-    "extract_tracklist",
-    "SoundCloudHandler",
-]
